5e252b59d0fe64a831811e2c529ce0c08b246bbf
yong.wang <yong.wang@ucarinc.com>
2020-04-16 22:46:17 +0800

    第一章节的练习题完毕

---
+++ src/ch01/Q04.go
@@ -0,0 +1,27 @@
+package ch01
+
+import (
+	"fmt"
+)
+
+func Q4() {
+	avg := t41([]float64{4.0, 1.0, 2.0, 3.0})
+	fmt.Println(avg)
+}
+
+func t41(xs []float64) float64 {
+
+	sum := 0.0
+	avg := 0.0
+
+	switch len(xs) {
+	case 0:
+		avg = 0.0
+	default:
+		for _, v := range xs {
+			sum += v
+		}
+		avg = sum / float64(len(xs))
+	}
+	return avg
+}
